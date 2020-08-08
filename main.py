3bb1c8462d8a93c0891efa8a21d340c19a008504
Harsh Agarwal <harsh29092000@gmail.com>
2020-08-08 15:56:57 +0530

    Added a screen

--- main.py
+++ main.py
@@ -3,3 +3,37 @@ import time
 import os
 import random
 
+pygame.init()
+width, height = 750, 7250
+WIN = pygame.display.set_mode((width, height))
+pygame.display.set_caption("SPACE SHOOTER")
+
+# LOAD SPACESHIPS
+RED_SPACESHIP = pygame.image.load(os.path.join("assets", "pixel_ship_red_small.png"))
+BLUE_SPACESHIP = pygame.image.load(os.path.join("assets", "pixel_ship_blue_small.png"))
+GREEN_SPACESHIP = pygame.image.load(os.path.join("assets", "pixel_ship_green_small.png"))
+YELLOW_SPACESHIP = pygame.image.load(os.path.join("assets", "pixel_ship_yellow.png"))
+
+# LOAD LASERS
+RED_LASER = pygame.image.load(os.path.join("assets", "pixel_laser_red.png"))
+BLUE_LASER = pygame.image.load(os.path.join("assets", "pixel_laser_blue.png"))
+GREEN_LASER = pygame.image.load(os.path.join("assets", "pixel_laser_green.png"))
+YELLOW_LASER = pygame.image.load(os.path.join("assets", "pixel_laser_yellow.png"))
+
+# LOAD BACKGROUND
+BG = pygame.image.load(os.path.join("assets", "background-black.png"))
+
+def main():
+    run = True
+    FPS = 60
+    clock = pygame.time.Clock()
+
+    while run:
+        clock.tick(FPS)
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+        pygame.display.flip()
+
+main()
