4ddd4aa5498dac2173c84bb5daa1af712a4719e9
Harsh Agarwal <harsh29092000@gmail.com>
2020-08-08 17:23:31 +0530

    Added background image and text

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ import os
 import random
 
 pygame.init()
-width, height = 750, 7250
-WIN = pygame.display.set_mode((width, height))
+WIDTH, HEIGHT = 750, 750
+WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("SPACE SHOOTER")
 
 # LOAD SPACESHIPS
@@ -21,19 +21,32 @@ GREEN_LASER = pygame.image.load(os.path.join("assets", "pixel_laser_green.png"))
 YELLOW_LASER = pygame.image.load(os.path.join("assets", "pixel_laser_yellow.png"))
 
 # LOAD BACKGROUND
-BG = pygame.image.load(os.path.join("assets", "background-black.png"))
+BG = pygame.transform.scale(pygame.image.load(os.path.join("assets", "background-black.png")), (WIDTH, HEIGHT))
 
 def main():
     run = True
     FPS = 60
+    score = 0
+    level = 1
+    lives = 5
+    main_font = pygame.font.SysFont("comicsans", size= 50)
+    
     clock = pygame.time.Clock()
-
+    def redraw_window():
+        WIN.blit(BG, (0,0))
+        # DRAW TEXT
+        level_label = main_font.render(f"Level: {level}", 1, (255,255,255))
+        lives_label = main_font.render(f"Lives: {lives}", 1, (255,255,255))
+        WIN.blit(lives_label, (10, 10))
+        WIN.blit(level_label, (WIDTH - level_label.get_width() - 10, 10))
+        pygame.display.update()
+    
     while run:
         clock.tick(FPS)
+        redraw_window()
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
-        pygame.display.flip()
 
 main()
